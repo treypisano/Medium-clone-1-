4f56f275b48ce9b929574dcd26ea3d3079ef3bf9
treypisano <treypisano@treypc>
2023-06-06 17:17:55 -0400

    repair seed data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
-    belongs_to :author,
-        class_name: :User,
-        foreign_key: :user_id
+    # belongs_to :author,
+    #     class_name: :User,
+    #     foreign_key: :user_id
 
     validates :title, presence: true
     validates :body, presence: true
